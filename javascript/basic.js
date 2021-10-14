ee9ea51d7841ac65d37fc4dea1e72a66d7cf835a
mohdrafeek <90964726+mohdrafeek@users.noreply.github.com>
2021-10-14 23:31:56 +0530

    update

--- javascript/basic.js
+++ javascript/basic.js
@@ -16,9 +16,20 @@ function display(){
 var b=0;
 function mode(){
     var about=document.getElementById("about");
-    var cont=document.getElementById("contact");
+    var contact=document.getElementById("contact");
     if(b%2==0)
     {
-
+        document.getElementById("mode").innerText="Light"
+        document.getElementById("mode").style.color="white"
+        about.style.backgroundColor="black"
+        contact.style.backgroundColor="rgb(59, 58, 58)"
+        b=b+1
+    }
+    else{
+        document.getElementById("mode").innerText="Dark"
+        document.getElementById("mode").style.color="black"
+        about.style.backgroundColor=""
+        contact.style.backgroundColor=""
+        b=b+1
     }
 }
